0752a75d935132203d8d87f42cb3f79424fd45ba
Andrew Craven <andrew@epworth-consulting.co.uk>
2025-07-27 15:41:58 +0000

    Replace string array with Scope class for scope tokens (#22)

--- app/Stackage.OAuth2.Fake.Tests/Model/UserAuthorizationTests.cs
+++ app/Stackage.OAuth2.Fake.Tests/Model/UserAuthorizationTests.cs
@@ -1,6 +1,5 @@
 namespace Stackage.OAuth2.Fake.Tests.Model;
 
-using System;
 using NUnit.Framework;
 using Stackage.OAuth2.Fake.Model;
 
@@ -10,10 +9,9 @@ public class UserAuthorizationTests
    public void scope_properties_equal_the_expected_values(ScopeTestCase testCase)
    {
       var testSubject = CreateAuthorization(
-         scope: testCase.GivenScope);
+         scope: (Scope)testCase.GivenScope);
 
-      Assert.That(testSubject.Scope, Is.EqualTo(testCase.ExpectedScope));
-      Assert.That(testSubject.Scopes, Is.EqualTo(testCase.ExpectedScopes));
+      Assert.That((string)testSubject.Scope, Is.EqualTo(testCase.ExpectedScope));
       Assert.That(testSubject.IncludeScope, Is.EqualTo(testCase.ExpectedIncludeScope));
       Assert.That(testSubject.IncludeRefreshToken, Is.EqualTo(testCase.ExpectedIncludeRefreshToken));
    }
@@ -25,56 +23,43 @@ public class UserAuthorizationTests
          new TestCaseData(new ScopeTestCase(
             GivenScope: string.Empty,
             ExpectedScope: string.Empty,
-            ExpectedScopes: [],
             ExpectedIncludeScope: false,
             ExpectedIncludeRefreshToken: false))
             .SetName("Empty"),
          new TestCaseData(new ScopeTestCase(
             GivenScope: "single_item",
             ExpectedScope: "single_item",
-            ExpectedScopes: ["single_item"],
             ExpectedIncludeScope: true,
             ExpectedIncludeRefreshToken: false))
             .SetName("Single scope"),
          new TestCaseData(new ScopeTestCase(
             GivenScope: "first_item second_item",
             ExpectedScope: "first_item second_item",
-            ExpectedScopes: ["first_item", "second_item"],
             ExpectedIncludeScope: true,
             ExpectedIncludeRefreshToken: false))
             .SetName("Two scopes"),
          new TestCaseData(new ScopeTestCase(
-            GivenScope: $" first_item {Environment.NewLine} second_item ",
-            ExpectedScope: "first_item second_item",
-            ExpectedScopes: ["first_item", "second_item"],
-            ExpectedIncludeScope: true,
-            ExpectedIncludeRefreshToken: false))
-            .SetName("Two scopes w/ whitespace"),
-         new TestCaseData(new ScopeTestCase(
             GivenScope: "offline_access",
             ExpectedScope: "offline_access",
-            ExpectedScopes: ["offline_access"],
             ExpectedIncludeScope: true,
             ExpectedIncludeRefreshToken: true))
             .SetName("Offline access alone"),
          new TestCaseData(new ScopeTestCase(
             GivenScope: "offline_access second_item",
             ExpectedScope: "offline_access second_item",
-            ExpectedScopes: ["offline_access", "second_item"],
             ExpectedIncludeScope: true,
             ExpectedIncludeRefreshToken: true))
             .SetName("Offline access first of two"),
          new TestCaseData(new ScopeTestCase(
             GivenScope: "first_item offline_access",
             ExpectedScope: "first_item offline_access",
-            ExpectedScopes: ["first_item", "offline_access"],
             ExpectedIncludeScope: true,
             ExpectedIncludeRefreshToken: true))
             .SetName("Offline access second of two"),
       ];
    }
 
-   private static UserAuthorization CreateAuthorization(string scope)
+   private static UserAuthorization CreateAuthorization(Scope scope)
    {
       return UserAuthorization.Create(scope: scope);
    }
@@ -82,7 +67,6 @@ public class UserAuthorizationTests
    public record ScopeTestCase(
       string GivenScope,
       string ExpectedScope,
-      string[] ExpectedScopes,
       bool ExpectedIncludeScope,
       bool ExpectedIncludeRefreshToken);
 }
--- app/Stackage.OAuth2.Fake/Model/UserAuthorization.cs
+++ app/Stackage.OAuth2.Fake/Model/UserAuthorization.cs
@@ -1,20 +1,17 @@
 namespace Stackage.OAuth2.Fake.Model;
 
 using System;
-using System.Linq;
 
-public record UserAuthorization(string Code, string[] Scopes) : IAuthorization
+public record UserAuthorization(string Code, Scope Scope) : IAuthorization
 {
-   public bool IncludeScope => Scopes.Length != 0;
+   public bool IncludeScope => !Scope.IsEmpty;
 
-   public bool IncludeRefreshToken => Scopes.Contains("offline_access");
+   public bool IncludeRefreshToken => Scope.Contains("offline_access");
 
-   public string Scope => string.Join(" ", Scopes);
-
-   public static UserAuthorization Create(string scope)
+   public static UserAuthorization Create(Scope scope)
    {
       return new UserAuthorization(
          Code: Guid.NewGuid().ToString(),
-         Scopes: scope.Split(' ').Where(s => !string.IsNullOrWhiteSpace(s)).ToArray());
+         Scope: scope);
    }
 }
