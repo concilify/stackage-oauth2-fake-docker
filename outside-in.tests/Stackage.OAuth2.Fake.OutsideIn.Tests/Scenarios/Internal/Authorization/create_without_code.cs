3ce059a80d2d52739ebc5c69c7c3106f53d4eb5f
acraven <andrew@epworth-consulting.co.uk>
2025-08-04 22:17:15 +0100

    wip

---
+++ outside-in.tests/Stackage.OAuth2.Fake.OutsideIn.Tests/Scenarios/Internal/Authorization/create_without_code.cs
@@ -0,0 +1,7 @@
+namespace Stackage.OAuth2.Fake.OutsideIn.Tests.Scenarios.Internal.Authorization;
+
+// ReSharper disable once InconsistentNaming
+public class create_without_code
+{
+
+}
---
+++ outside-in.tests/Stackage.OAuth2.Fake.OutsideIn.Tests/Scenarios/Internal/RefreshToken/create_without_body.cs
@@ -0,0 +1,7 @@
+namespace Stackage.OAuth2.Fake.OutsideIn.Tests.Scenarios.Internal.RefreshToken;
+
+// ReSharper disable once InconsistentNaming
+public class create_without_body
+{
+
+}
