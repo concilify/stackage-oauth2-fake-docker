99e3a357f651a3128a036c6c477a332acc8bf485
acraven <andrew@epworth-consulting.co.uk>
2025-12-31 13:30:46 +0000

    tidy

--- app/Stackage.OAuth2.Fake/Model/Authorization/IAuthorizationWithIdToken.cs
+++
@@ -1,6 +0,0 @@
-namespace Stackage.OAuth2.Fake.Model.Authorization;
-
-public interface IAuthorizationWithIdToken
-{
-   string ClientId { get; }
-}
--- app/Stackage.OAuth2.Fake/Model/Authorization/IAuthorizationWithRefreshToken.cs
+++
@@ -1,5 +0,0 @@
-namespace Stackage.OAuth2.Fake.Model.Authorization;
-
-public interface IAuthorizationWithRefreshToken
-{
-}
